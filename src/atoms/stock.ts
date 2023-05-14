322ec75bf5ee993e59c43522ee02007eb2d5ab6d
YunSeong Park <qkrdbstjd95@naver.com>
2023-05-14 17:08:26 +0900

    [New Feature] stock atom 작성

---
+++ src/atoms/stock.ts
@@ -0,0 +1,37 @@
+import { atom } from "jotai";
+
+export const currentStockAtom = atom<string | undefined>(undefined);
+export const setCurrentStockAtom = atom(null, (get, set, update: string) => {
+  const idList = get(stockIdListAtom);
+
+  if (idList.includes(update)) {
+    set(currentStockAtom, update);
+  }
+});
+
+export const stockIdListAtom = atom<string[]>([]);
+
+export const deleteStockAtom = atom(null, (get, set, id: string) => {
+  const prev = get(stockIdListAtom);
+  const idx = prev.indexOf(id);
+  const update = [...prev];
+
+  if (idx !== -1) {
+    update.splice(idx, 1);
+    const currentStock = get(currentStockAtom);
+    if (currentStock === id) {
+      set(currentStockAtom, undefined);
+    }
+  }
+
+  set(stockIdListAtom, update);
+});
+export const addStockAtom = atom(null, (get, set, id: string) => {
+  const prev = get(stockIdListAtom);
+  const update = [...prev];
+
+  update.push(id);
+
+  set(currentStockAtom, id);
+  set(stockIdListAtom, update);
+});
