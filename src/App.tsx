76dbdcdc3cac3ae8f2e44f543c5a14041b10b581
YunSeong Park <qkrdbstjd95@naver.com>
2023-05-23 19:27:28 +0900

    [New Feature] app컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import StockTab from "./components/StockTab/StockTab";
+import StockInfoForm from "./components/StockInfoForm/StockInfoForm";
+import KellyCurve from "./components/KellyCurve/KellyCurve";
+import { css } from "@emotion/react";
+import Notify from "./components/Notify/Notify";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StockTab />
+      <main
+        css={css`
+          display: flex;
+        `}
+      >
+        <StockInfoForm />
+        <KellyCurve />
+        <Notify />
+      </main>
+    </div>
+  );
 }
 
 export default App;
