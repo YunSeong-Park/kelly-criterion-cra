2319618a2e17bcad4e1aab9af45e51770a17316e
YunSeong Park <qkrdbstjd95@naver.com>
2023-05-23 19:37:37 +0900

    [New Feature] color primary, gray, white 추가

---
+++ src/styles/color.style.ts
@@ -0,0 +1,16 @@
+const colors = {
+  primary: {
+    600: "#7F56D9",
+  },
+  gray: {
+    200: "#EAECF0",
+    300: "#D0D5DD",
+    500: "#667085",
+    600: "#475467",
+    700: "#344054",
+    900: "#101828",
+  },
+  white: "#ffffff",
+} as const;
+
+export default colors;
