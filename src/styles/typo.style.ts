00b0000f1843784932694059df449c4ac6df1c10
YunSeong Park <qkrdbstjd95@naver.com>
2023-05-29 18:29:42 +0900

    [New Feature] typo 공통 style 추가

--- src/styles/typo.style.ts
+++ src/styles/typo.style.ts
@@ -1,20 +1,28 @@
 import { css } from "@emotion/react";
 import colors from "./color.style";
 
+const commonStyle = css`
+  padding: 0px;
+  margin: 0px;
+`;
+
 const header = {
   large: css`
+    ${commonStyle.styles}
     font-size: 24px;
     font-weight: 600;
     color: ${colors.gray[900]};
     line-height: 32px;
   `,
   medium: css`
+    ${commonStyle.styles}
     font-weight: 700;
     font-size: 14px;
     line-height: 20px;
     color: ${colors.gray[700]};
   `,
   medium_smibold: css`
+    ${commonStyle.styles}
     font-weight: 500;
     font-size: 14px;
     line-height: 20px;
@@ -24,12 +32,14 @@ const header = {
 
 const subText = {
   large: css`
+    ${commonStyle.styles}
     font-size: 16px;
     line-height: 24px;
     font-weight: 400;
     color: ${colors.gray[600]};
   `,
   medium: css`
+    ${commonStyle.styles}
     font-size: 12px;
     line-height: 20px;
     font-weight: 400;
@@ -39,18 +49,21 @@ const subText = {
 
 const body = {
   medium: css`
+    ${commonStyle.styles}
     font-weight: 400;
     font-size: 16px;
     line-height: 24px;
     color: ${colors.gray[500]};
   `,
   medium_semibold: css`
+    ${commonStyle.styles}
     font-weight: 600;
     font-size: 14px;
     line-height: 20px;
     color: ${colors.gray[600]};
   `,
   small: css`
+    ${commonStyle.styles}
     font-weight: 500;
     font-size: 12px;
     line-height: 20px;
